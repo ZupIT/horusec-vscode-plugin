fa5852cad01ab4fc47975493104a452f2d7ef2cb
nathan <nathan.martins@zup.com.br>
2020-12-10 10:23:56 -0300

    Removing stdout parser

--- horusec-vscode/src/extension.ts
+++ horusec-vscode/src/extension.ts
@@ -2,7 +2,7 @@ import * as vscode from 'vscode';
 import { exec } from 'child_process';
 import { v4 as uuidv4 } from 'uuid';
 import { subscribeToDocumentChanges } from './util/diagnostics';
-import { parseStdoutToVulnerabilities, removeCertMessages } from './util/parser';
+import { parseOutputToAnalysis } from './util/parser';
 
 const vulnDiagnostics = vscode.languages.createDiagnosticCollection("vulnerabilities");
 
@@ -17,9 +17,6 @@ export function activate(context: vscode.ExtensionContext) {
 	}));
 }
 
-/**
- * Validates workspace and show message that analysis has started
- */
 function runHorusec() {
 	if (vscode.workspace.rootPath === undefined) {
 		vscode.window.showErrorMessage('No valid workspace found.');
@@ -30,10 +27,6 @@ function runHorusec() {
 	execStartCommand();
 }
 
-/**
- * Execute horusec docker cli image start command in actual workspace
- * Show information message that analysis has finished
- */
 function execStartCommand() {
 	exec(getCommand(), (error: any, stdout: any) => {
 		if (error) {
@@ -42,60 +35,34 @@ function execStartCommand() {
 			return;
 		}
 
-		updateVulnDiagnotics(stdout);
-		openFileWithResult(removeCertMessages(stdout));
+		updateVulnDiagnotics();
 		vscode.window.showInformationMessage(`Analysis finished with success!`);
 	});
 }
 
-/**
- * Open a file in vscode with the analysis results
- * @param stdout horusec cli container output
- */
-function openFileWithResult(stdout: string) {
-	try {
-		vscode.workspace.openTextDocument({
-			language: 'text',
-			content: stdout
-		}).then(doc => {
-			vscode.window.showTextDocument(doc);
-		});
-
-	} catch (error) {
-		console.log(error);
-	}
-}
-
-/**
- * Update code diagnotics after parsing stdout to vulnerabilities
- * @param stdout horusec cli container output
- */
-function updateVulnDiagnotics(stdout: string) {
+function updateVulnDiagnotics() {
 	try {
 		vulnDiagnostics.clear();
 
 		subscribeToDocumentChanges(
 			vulnDiagnostics,
-			parseStdoutToVulnerabilities(stdout)
+			parseOutputToAnalysis()
 		);
 	} catch (error) {
 		console.log(error);
 	}
 }
 
-/**
- * Command that will be execute horusec docker image of cli
- */
 function getCommand(): string {
 	const dockerSock = `-v /var/run/docker.sock:/var/run/docker.sock`;
 	const startCommandUUID = uuidv4();
 	const analysisFolder = `/src/horusec-vscode-${startCommandUUID}`;
 	const bindVolume = `-v ${vscode.workspace.rootPath}:${analysisFolder}`;
 	const cliImage = `horuszup/horusec-cli:v1.6.0-alpha-1`;
-	const horusecStart = `horusec start -p ${analysisFolder} -P ${vscode.workspace.rootPath}`;
+	const horusecJsonOutput = `-o json -O ${analysisFolder}/horusec-result.json`;
+	const horusecStart = `horusec start -p ${analysisFolder} -P ${vscode.workspace.rootPath} ${horusecJsonOutput}`;
 
 	return `docker run ${dockerSock} ${bindVolume} ${cliImage} ${horusecStart}`;
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() { }
