16281d494481b1345bfad1b6f9d81f30a5ae7362
lucas.bruno <lucas.bruno@zup.com.br>
2021-01-21 10:08:21 -0300

    Added settins option to configure flags of horusec configuration file

--- horusec-vscode/src/entities/horusecConfig.ts
+++ horusec-vscode/src/entities/horusecConfig.ts
@@ -1,4 +1,28 @@
+/* eslint-disable */
 export interface HorusecConfigFile {
+  horusecCliMonitorRetryInSeconds?: number;
+  horusecCliPrintOutputType?: string;
+  horusecCliJsonOutputFilepath?: string;
+  horusecCliTypesOfVulnerabilitiesToIgnore?: string;
+  horusecCliFilesOrPathsToIgnore?: string;
+  horusecCliHorusecApiUri?: string;
+  horusecCliTimeoutInSecondsRequest?: number;
+  horusecCliTimeoutInSecondsAnalysis?: number;
+  horusecCliRepositoryAuthorization?: string;
+  horusecCliReturnErrorIfFoundVulnerability?: boolean;
+  horusecCliProjectPath?: string;
+  horusecCliCertInsecureSkipVerify?: boolean;
+  horusecCliCertPath?: string;
+  horusecCliFilterPath?: string;
+  horusecCliEnableGitHistoryAnalysis?: boolean;
+  horusecCliEnableCommitAuthor?: boolean;
+  horusecCliRepositoryName?: string;
+  horusecCliCustomRulesPath?: string;
+  horusecCliEnableInformationSeverity?: boolean;
+  EnvContainerBindProjectPath?: string;
+  horusecCliHeaders?: string;
+  horusecCliWorkDir?: any;
+  horusecCliToolsConfig?: any;
   horusecCliFalsePositiveHashes?: string[];
   horusecCliRiskAcceptHashes?: string[];
 }
